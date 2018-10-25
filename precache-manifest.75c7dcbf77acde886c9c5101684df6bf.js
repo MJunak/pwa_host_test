593252ea9172fb448bdc12a423de75e916608d7d
krz <marius.junak@scorpius-forge.de>
2018-10-25 10:33:24 +0200

    initial commit

---
+++ precache-manifest.75c7dcbf77acde886c9c5101684df6bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3dd1ac9d652de9e494e1",
+    "url": "/static/js/main.3dd1ac9d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "3dd1ac9d652de9e494e1",
+    "url": "/static/css/main.9a34a238.chunk.css"
+  },
+  {
+    "revision": "1ced57c9735efc78437f92223a0d74a5",
+    "url": "/index.html"
+  }
+];
